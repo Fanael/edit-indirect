a28aba75f86f16f23de8024f51eb2fd09bcacd31
Fanael Linithien <fanael4@gmail.com>
2014-12-13 19:03:11 +0100

    Don't separate sentences with double spaces.

--- edit-indirect.el
+++ edit-indirect.el
@@ -337,7 +337,7 @@ called with updated positions."
   ;; Kill the overlay reference so that `edit-indirect--abort-on-kill-buffer'
   ;; won't try to call us again.
   (setq edit-indirect--overlay nil)
-  ;; If we created a window, get rid of it.  Kill the buffer we created.
+  ;; If we created a window, get rid of it. Kill the buffer we created.
   (quit-window t))
 
 (defun edit-indirect--abort-on-kill-buffer ()
