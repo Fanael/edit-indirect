215d3a714a08c25b533ad238ba2aa905321b6009
Fanael Linithien <fanael4@gmail.com>
2016-05-28 15:32:04 +0200

    Release version 0.1.3

--- edit-indirect.el
+++ edit-indirect.el
@@ -2,7 +2,7 @@
 
 ;; Author: Fanael Linithien <fanael4@gmail.com>
 ;; URL: https://github.com/Fanael/edit-indirect
-;; Version: 0.1.2
+;; Version: 0.1.3
 ;; Package-Requires: ((emacs "24.3"))
 
 ;; This file is NOT part of GNU Emacs.
