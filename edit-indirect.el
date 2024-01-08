Fanael/edit-indirect

commit 7a3c1b2b5fa88cfc5f02f9d0fcc0caed83209f1f

Fix cleanup when indirect buffer is not selected
When the indirect buffer is killed but not selected, the current window
and buffer are killed. Now we check in the `kill-buffer-hook` if the
cleanup function is being ran interactively. Also, now before quitting the
window, we check that we are in fact currently selecting the
edit-indirect buffer.

edit-indirect.el
@@ -212,7 +212,7 @@ Can be called only when the current buffer is an edit-indirect
 buffer."
   (interactive)
   (edit-indirect--barf-if-not-indirect)
-  (edit-indirect--abort))
+  (edit-indirect--abort t))
 
 (defun edit-indirect-buffer-indirect-p (&optional buffer)
   "Non-nil iff the BUFFER is an edit-indirect buffer.
@@ -396,20 +396,18 @@ called with updated positions."
     (when beg-marker (set-marker beg-marker nil))
     (when end-marker (set-marker end-marker nil))))
 
-(defun edit-indirect--abort ()
-  "Abort indirect edit."
-  (edit-indirect--clean-up))
-
-(defun edit-indirect--clean-up ()
-  "Clean up an edit-indirect buffer."
+(defun edit-indirect--abort (kill)
+  "Abort an indirect edit and clean up the edit-indirect buffer."
   (delete-overlay edit-indirect--overlay)
   ;; Kill the overlay reference so that `edit-indirect--abort-on-kill-buffer'
   ;; won't try to call us again.
   (setq edit-indirect--overlay nil)
   ;; If we created a window, get rid of it. Kill the buffer we created.
-  (if edit-indirect--should-quit-window
-      (quit-window t)
-    (kill-buffer)))
+  (if (and edit-indirect--should-quit-window
+           (eq (window-buffer (selected-window))
+               (current-buffer)))
+      (quit-window kill)
+    (and kill (kill-buffer))))
 
 (defun edit-indirect--rebind-save-hooks ()
   "Bind our `save-buffer' hooks in the current buffer.
@@ -428,7 +426,7 @@ Should only be called from `write-contents-functions'."
   "Abort indirect edit.
 Should be called only from `kill-buffer-hook'."
   (when edit-indirect--overlay
-    (edit-indirect--abort)))
+    (edit-indirect--abort nil)))
 
 (defun edit-indirect--barf-if-not-indirect ()
   "Signal an error if the current buffer is not an edit-indirect buffer.

commit d0378d9747791fcd78370a0e52ec018d5970c482

Fix call to undefined edit-indirect--clean-up in edit-indirect-commit
Fixes regression introduced in 7a3c1b2b5fa88cfc5f02f9d0fcc0caed83209f1f.

edit-indirect.el
@@ -2,14 +2,14 @@
 
 ;; Author: Fanael Linithien <fanael4@gmail.com>
 ;; URL: https://github.com/Fanael/edit-indirect
-;; Version: 0.1.10
+;; Version: 0.1.11
 ;; Package-Requires: ((emacs "24.3"))
 
 ;; This file is NOT part of GNU Emacs.
 
 ;; SPDX-License-Identifier: BSD-2-clause
 ;;
-;; Copyright (c) 2014-2022, Fanael Linithien
+;; Copyright (c) 2014-2024, Fanael Linithien
 ;; All rights reserved.
 ;;
 ;; Redistribution and use in source and binary forms, with or without
@@ -192,7 +192,7 @@ buffer."
   (interactive)
   (edit-indirect--barf-if-not-indirect)
   (edit-indirect--commit)
-  (edit-indirect--clean-up))
+  (edit-indirect--abort t))
 
 (defun edit-indirect-save ()
   "Save the modifications done in an edit-indirect buffer.